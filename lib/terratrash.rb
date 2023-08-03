f6a103734a40d8c1b4336ccec75d443946a173c7
grantbirki <grantbirki@github.com>
2023-08-03 17:17:06 +0100

    init

--- lib/terratrash.rb
+++ lib/terratrash.rb
@@ -1,7 +1,65 @@
 # frozen_string_literal: true
 
-module Terratrash
-  def self.todo
-    true
+require "logger"
+
+class Terratrash
+  def initialize(logger: nil)
+    @log = logger || Logger.new($stdout, level: ENV.fetch("LOG_LEVEL", "INFO").upcase)
+  end
+
+  def clean(terraform)
+    # split the output into an array of lines
+    terraform_array = terraform.split("\n")
+
+    # remove any items from the array that include any bits of the following strings
+    terraform_array.delete_if { |item| item.include?("Reading...") }
+    terraform_array.delete_if { |item| item.include?("Refreshing state...") }
+    terraform_array.delete_if { |item| item.include?("Read complete") }
+    terraform_array.delete_if { |item| item.include?("Still reading...") }
+    terraform_array.delete_if { |item| item.include?("╷") }
+    terraform_array.delete_if { |item| item.include?("│") }
+    terraform_array.delete_if { |item| item.include?("╵") }
+    terraform_array.delete_if { |item| item.include?("::debug::") }
+    terraform_array.delete_if { |item| item.include?("[command]/home/runner/work/") }
+
+    begin
+      # find what position in the array the line "Initializing plugins and modules..." is at
+      initializing_position = terraform_array.index("Initializing plugins and modules...")
+
+      # if the line is not found, print a warning
+      if initializing_position.nil?
+        puts "Warning: Could not find the line 'Initializing plugins and modules...' in the Terraform output"
+      else
+        # remove all the lines from the array up to that position
+        terraform_array.slice!(0..initializing_position)
+        # if the very first line is now empty or a newline, remove it as well
+        terraform_array.delete_at(0) if terraform_array[0].strip == ""
+      end
+    rescue StandardError => e
+      puts "Warning: #{e}"
+      puts "Could not find the line 'Initializing plugins and modules...' in the Terraform output"
+      puts "moving on..."
+    end
+
+    # re-join the array into a string
+    output = terraform_array.join("\n")
+
+    # removing terraform warnings to clean things up for reviewers
+    output.gsub!(/Warning: Experimental feature.*?similar warnings elsewhere\)/m, "")
+    # removing terraform plan -out note
+    output.gsub!(/Note: You didn't use the -out option.*?actions if you run "terraform apply" now./m, "")
+
+    # remove any leading newline(s) characters from the beginning of the string
+    output.gsub!(/\A\n*/, "")
+    # remove any trailing newline(s) characters from the end of the string
+    output.gsub!(/\n*\z/, "")
+    # if the output ends with three or more '─' characters, remove them
+    output.gsub!(/─{3,}\z/, "")
+    # again, remove any trailing newline(s) characters from the end of the string
+    output.gsub!(/\n*\z/, "")
+    # if three or more consecutive newline characters are found, replace them with one newline character
+    output.gsub!(/\n{3,}/, "\n")
+
+    return output
   end
 end
--- spec/lib/terratrash_spec.rb
+++ spec/lib/terratrash_spec.rb
@@ -4,9 +4,12 @@ require "spec_helper"
 require_relative "../../lib/terratrash"
 
 describe Terratrash do
-  context "#" do
-    it "todo" do
-
+  context "#initialize" do
+    it "creates a Terratrash class" do
+      terratrash = described_class.new
+      expect(terratrash).to be_a(Terratrash)
+      expect(terratrash).to respond_to(:clean)
+      expect(terratrash.instance_variable_get(:@log)).to be_a(Logger)
     end
   end
 end
